2042d175bea27cab71604e8211c6c8ccd25509a3
Rodrigo “Albo” Arce <albo@pragmore.com>
2022-03-09 11:14:47 -0300

    Clean code

--- worker.js
+++ worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', function (event) {
   event.waitUntil(
-    caches.open('v1').then(function (cache) {
+    caches.open('v2').then(function (cache) {
       return cache.addAll([
         './',
         './index.html'
@@ -16,7 +16,7 @@ self.addEventListener('fetch', function (event) {
     } else {
       return fetch(event.request).then(function (response) {
         const responseClone = response.clone()
-        caches.open('v1').then(function (cache) {
+        caches.open('v2').then(function (cache) {
           cache.put(event.request, responseClone)
         })
         return response
