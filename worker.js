ed1a0f455030dae86f4a2e59d602aa8da72a969f
Rodrigo “Albo” Arce <albo@pragmore.com>
2022-03-12 14:37:22 -0300

    Fix arbitrary code run with JSFuck

--- worker.js
+++ worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', function (event) {
   event.waitUntil(
-    caches.open('v2').then(function (cache) {
+    caches.open('v3').then(function (cache) {
       return cache.addAll([
         './',
         './index.html'
